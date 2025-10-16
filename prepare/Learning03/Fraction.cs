2d5fa763c88345d43046a82dd27c79168bec2f77
JaedenP2003 <jaeden.platt2003@gmail.com>
2025-10-16 14:50:04 -0600

    Finished Learning 3

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,37 @@
+using System;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    // Constructors
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    // Methods
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / _bottom;
+    }
+}

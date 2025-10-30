6689393c30c493e6ca28aebf307dc22dffab0404
JaedenP2003 <jaeden.platt2003@gmail.com>
2025-10-30 08:51:02 -0600

    Finished Learning 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Issac Cottrell", "Subtraction");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Samuel Powell", "Fractions", "8.9", "21-28");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Liz Platt", "American History", "The Causes of the Revolutionary War");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
